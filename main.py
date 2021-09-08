cff1a2758d522f652dbea98d06fc8a31a8ebc99a
MingoDynasty <github@mingodynasty.com>
2021-09-07 20:50:02 -0700

    Use YAML logging.

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import logging.config
 import os
-import sched
 import sys
+
+import sched
 import time
 
 import config
+import yaml
 from database import DatabaseController
 from reddit_controller import RedditController
 
 if __name__ == '__main__':
-    logConfFile = os.path.join(os.path.dirname(__file__), 'logging.conf')
+    logConfFile = os.path.join(os.path.dirname(__file__), 'logging.yaml')
     if not os.path.isfile(logConfFile):
         print(logConfFile + " not found.")
         sys.exit()
 
     LOG_FORMAT = "%(asctime)-15s - %(levelname)s - %(message)s"
     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format=LOG_FORMAT)
-    logging.config.fileConfig(logConfFile)
+
+    with open(logConfFile, 'r') as fh:
+        logging_config = yaml.safe_load(fh.read())
+        logging.config.dictConfig(logging_config)
+
     logger = logging.getLogger(__name__)
 
     databaseController = DatabaseController()
+    databaseController.move_db()
     databaseController.setup_db()
 
     redditController = RedditController()
